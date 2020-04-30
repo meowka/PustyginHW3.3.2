e03cceec5fdc22bb56fcc30d57b07f49b0153b2a
meowka <Pustygin.a@mail.ru>
2020-04-30 13:57:09 +0700

    refactor

--- app/src/main/java/com/example/hw332/MainActivity.java
+++ app/src/main/java/com/example/hw332/MainActivity.java
@@ -3,51 +3,80 @@ package com.example.hw332;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
-    String [] language = {" ", "ru", "en"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String lang = getString(R.string.language);
+        String [] language = {lang, "ru", "en"};
         Button button = findViewById(R.id.button);
         final Spinner spinner = findViewById(R.id.spinner);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, language);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, language){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setPrompt(getString(R.string.language));
-
 
        button.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-               if (spinner.getSelectedItem().toString().equals("ru")){
-                           Locale locale = new Locale("ru");
-                           Configuration config = new Configuration();
-                           config.setLocale(locale);
-                           getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-                           recreate();
-           }
-               else if (spinner.getSelectedItem().toString().equals("en")){
-                   Locale locale = new Locale("en");
-                   Configuration config = new Configuration();
-                   config.setLocale(locale);
-                   getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-                   recreate();
+               switch (spinner.getSelectedItem().toString()) {
+                   case "ru": {
+                       changeLocale("ru");
+                       break;
+                   }
+                   case "en": {
+                       changeLocale("en");
+                       break;
+                   }
                }
            }
        });
+    }
+
+    private void changeLocale(String l) {
+        Locale locale = new Locale(l);
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        recreate();
     }
 }
